232f6e3ad07678948044cee67f89abfad5eec409
Rifky Adiansyah <adiansyah.rfky@gmail.com>
2026-01-23 21:36:39 +0700

    update fitur X

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./*.{html,js}", "./page/**/*.{html,js}"],
+  content: ["./*.{html,js}", "./public/page/**/*.{html,js}"],
   theme: {
     extend: {
       colors: {
